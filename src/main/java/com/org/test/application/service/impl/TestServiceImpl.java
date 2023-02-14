b68c53ecd7527902166db98fb69df74c896bc71d
shin-yonghun <csyqwe9333@gmail.com>
2023-02-14 18:30:15 +0900

    @transctional test

--- src/main/java/com/org/test/application/service/impl/TestServiceImpl.java
+++ src/main/java/com/org/test/application/service/impl/TestServiceImpl.java
@@ -6,6 +6,8 @@ import com.org.test.application.service.TestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
@@ -15,20 +17,35 @@ public class TestServiceImpl implements TestService {
     private final StudentRepository studentRepository;
 
     @Override
-    @Transactional
+    @Transactional(rollbackFor = Exception.class, // 모든 예외상황에 대해 롤백 (기본적으로 Unchecked Exception, Error 만을 rollback)
+            readOnly = false, // 읽기 전용 off
+            isolation = Isolation.DEFAULT, // 격리수준
+            // DEFAULT : DBMS 의 기본 격리 수준 이용
+            // READ_UNCOMMITED (level 0) : Dirty read, Nonrepeatable read, Phantom read 모두발생
+            // READ_COMMITED (level 1) : Dirty read 방지, Postgres, SQL Server 및 Oracle의 기본 수준
+            // REPEATEABLE_READ (level 2) : Dirty read, Nonrepeatable read 방지, MySQL의 기본 수준, Oracle은 미지원
+            // SERIALIZABLE (level 3) : 모든 부작용을 방지, 가장 높은 격리 수준이지만, 동시 호출을 순차적으로 실행하므로 성능 저하의 우려
+            propagation = Propagation.REQUIRED) // 전파속성
+            // REQUIRED (default) : 활성 트랜잭션이 있는지 확인하고, 아무것도 없으면 새 트랜잭션을 생성
+            // SUPPORTS : 활성 트랜잭션이 있는지 확인하고, 있으면 기존 트랜잭션 사용. 없으면 트랜잭션 없이 실행
+            // REQUIRES_NEW : 항생 새로운 트랜잭션을 생성한다. 이미 진행중인 트랜잭션이 있다면 잠깐 보류하고 해당 트랜잭션 작업을 먼저 진행
+            // NESTED : 부모 트랜잭션이 커밋될 때 같이 커밋, 자식 트랜잭션의 롤백은 부모 트랜잭션에 영향 없음
     public void transactionalTest() {
-        Student s1 = new Student();
-        s1.setName("test");
-        s1.setAge(19);
-        s1.setGender(1);
-        studentRepository.save(s1);
+        Student student = studentRepository.findById(1L).get();
+        trans1(student);
+
     }
 
-    public int trans1(){
-        return 1;
+    @Transactional(propagation = Propagation.NESTED)
+    public void trans1(Student student){
+        student.setAge(18);
+        trans2(student);
+        studentRepository.save(student);
     }
 
-    private int trans2(){
-        return 2;
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    private void trans2(Student student){
+        student.setAge(17);
+        studentRepository.save(student);
     }
 }
--- src/test/java/com/org/test/application/controller/TestControllerTest.java
+++ src/test/java/com/org/test/application/controller/TestControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,8 +14,6 @@ import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
